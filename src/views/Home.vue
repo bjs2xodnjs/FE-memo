c5f0d2774ae88c720b1713852414b520324679ee
taewon1918 <taewon1918@gmail.com>
2025-06-25 18:10:24 +0900

    [docs] 프론트 메모 6.25

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { reactive, onMounted } from 'vue';
 import { HttpService } from '@/services/HttpService';
+import { HttpStatusCode } from 'axios';
 
 const httpService = new HttpService();
 
@@ -8,23 +9,33 @@ const state = reactive({
   memos: []
 })
 
-onMounted(async () =>{
-  state.memos = await httpService.getItems();
+onMounted(() =>{
+  getItems();
 });
+const getItems = async () => {
+  state.memos = await httpService.getItems();
+}
+const remove = async id => {
+  console.log ("id", id);
+  const result = await httpService.delItem(id);
+  if(result === '성공') {
+    getItems();
+  }
+}  
 </script>
 
 <template>
     <div class="memo-list">
-    <router-link v-for="m in state.memos" :to="`/memos/${m.id}`" class="item" :key="m.id">
+    <router-link v-for="m in state.memos" :to="`memos/${m.id}`" class="item" :key="m.id">
       <div class="d-flex pt-3">
         <div class="pb-3 mb-0 w-100">
           <div class="d-flex justify-content-between">
             <b> {{ m.title }}</b>
             <div>
-              <span role="button">삭제</span>
+              <span role="button" @click.prevent="remove(m.id)">삭제</span>
             </div>
           </div>
-          <div class="mt-2">{{ m.content }}</div>>
+          <div class="mt-2">{{ m.content }}</div>
         </div>
       </div>
     </router-link>
